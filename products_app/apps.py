ccd9935f1d8db28f02a1e373b50b2b207fd28e88
Sergey <smirsand@mail.ru>
2023-08-29 18:20:41 +0300

    add code

--- products_app/apps.py
+++ products_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ProductsAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products_app'
+    verbose_name = 'Продукты'
